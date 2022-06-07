81fca3f194355a1a0f88d21de0d90bb1fc404e5a
Dmitriy <yurchenkoo.dev@gmail.com>
2022-06-07 16:33:13 +0200

    Add contact list app

---
+++ hometask/6/__main.py
@@ -0,0 +1,15 @@
+# Split Contact list program for HW 4 by packages
+# Extend contact list program. Add phone and email validation for user input
+# Add validation for name. Name length can not be less than 3 letters
+# Add delete contact functionality
+# Add tabular output
+from contact_list import App
+
+
+def main():
+    app = App()
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
